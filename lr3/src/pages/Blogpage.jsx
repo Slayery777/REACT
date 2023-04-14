5dc8eb1a904bc8ca33a91bffc20220d9900cbfaf
Slayery777 <vladislavbrus1@gmail.com>
2023-04-14 23:04:47 +0300

    lr3 adding blog, extend mainpage

--- lr3/src/pages/Blogpage.jsx
+++ lr3/src/pages/Blogpage.jsx
@@ -4,7 +4,49 @@ import { Link, useSearchParams } from 'react-router-dom';
 const Blogpage = () => {
     return (
         <div>
-            
+        <div class="container my-4">
+        <h1>Блог</h1>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Назва поста</h2>
+            <p>Текст поста. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet facilisis lorem. Sed ullamcorper dictum velit at euismod. Sed accumsan aliquet mauris. Ut in egestas ipsum, ut bibendum sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec pretium mi neque, vel consectetur tellus tincidunt id. Morbi fermentum tellus sit amet arcu tincidunt scelerisque. Vivamus eget ex ac purus viverra varius. Duis sit amet lobortis turpis. Suspendisse semper tortor elit, ac tincidunt lorem eleifend vel. Phasellus ultricies nisi sed sapien elementum, ut tincidunt est vestibulum. Nam in consequat mi. Sed venenatis sapien ac arcu suscipit feugiat. Sed auctor convallis felis id varius. Nunc in nibh ex.</p>
+            </div>
+        </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Назва поста</h2>
+            <p>Текст поста. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet facilisis lorem. Sed ullamcorper dictum velit at euismod. Sed accumsan aliquet mauris. Ut in egestas ipsum, ut bibendum sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec pretium mi neque, vel consectetur tellus tincidunt id. Morbi fermentum tellus sit amet arcu tincidunt scelerisque. Vivamus eget ex ac purus viverra varius. Duis sit amet lobortis turpis. Suspendisse semper tortor elit, ac tincidunt lorem eleifend vel. Phasellus ultricies nisi sed sapien elementum, ut tincidunt est vestibulum. Nam in consequat mi. Sed venenatis sapien ac arcu suscipit feugiat. Sed auctor convallis felis id varius. Nunc in nibh ex.</p>
+            </div>
+        </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Назва поста</h2>
+            <p>Текст поста. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet facilisis lorem. Sed ullamcorper dictum velit at euismod. Sed accumsan aliquet mauris. Ut in egestas ipsum, ut bibendum sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec pretium mi neque, vel consectetur tellus tincidunt id. Morbi fermentum tellus sit amet arcu tincidunt scelerisque. Vivamus eget ex ac purus viverra varius. Duis sit amet lobortis turpis. Suspendisse semper tortor elit, ac tincidunt lorem eleifend vel. Phasellus ultricies nisi sed sapien elementum, ut tincidunt est vestibulum. Nam in consequat mi. Sed venenatis sapien ac arcu suscipit feugiat. Sed auctor convallis felis id varius. Nunc in nibh ex.</p>
+            </div>
+        </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Назва поста</h2>
+            <p>Текст поста. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet facilisis lorem. Sed ullamcorper dictum velit at euismod. Sed accumsan aliquet mauris. Ut in egestas ipsum, ut bibendum sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec pretium mi neque, vel consectetur tellus tincidunt id. Morbi fermentum tellus sit amet arcu tincidunt scelerisque. Vivamus eget ex ac purus viverra varius. Duis sit amet lobortis turpis. Suspendisse semper tortor elit, ac tincidunt lorem eleifend vel. Phasellus ultricies nisi sed sapien elementum, ut tincidunt est vestibulum. Nam in consequat mi. Sed venenatis sapien ac arcu suscipit feugiat. Sed auctor convallis felis id varius. Nunc in nibh ex.</p>
+            </div>
+        </div>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <h2>Назва поста</h2>
+            <p>Текст поста. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet facilisis lorem. Sed ullamcorper dictum velit at euismod. Sed accumsan aliquet mauris. Ut in egestas ipsum, ut bibendum sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec pretium mi neque, vel consectetur tellus tincidunt id. Morbi fermentum tellus sit amet arcu tincidunt scelerisque. Vivamus eget ex ac purus viverra varius. Duis sit amet lobortis turpis. Suspendisse semper tortor elit, ac tincidunt lorem eleifend vel. Phasellus ultricies nisi sed sapien elementum, ut tincidunt est vestibulum. Nam in consequat mi. Sed venenatis sapien ac arcu suscipit feugiat. Sed auctor convallis felis id varius. Nunc in nibh ex.</p>
+            </div>
+        </div>
+        </div>
         </div>
     )
 }

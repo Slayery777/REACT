f3e3da5d5df3d88b66eed9ad7d30928bd25c3668
Slayery777 <vladislavbrus1@gmail.com>
2023-04-14 22:44:09 +0300

    react fix

--- lr2/src/index.js
+++ lr2/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom'
@@ -17,22 +16,3 @@ root.render(
   </StrictMode>,
   document.getElementById('root')
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> b0dd1071662b997505d112bf1a30e2272b35ea45
